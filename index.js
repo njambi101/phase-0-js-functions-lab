9f6ff1a49d2d886fbaf3b7c73cc8cb83e1968699
njambi101 <grace.nganga@student.moringaschool.com>
2026-09-25 00:41:19 +0300

    function project

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
+function calculateTax (amount) {
+    let calc = amount * 10 /100;
+    return calc
+}
 
+function convertToUpperCase(text) {
+    let upperCase = text.toUpperCase();
+    return upperCase;
+}
 
+function findMaximum(num1, num2) {
+    if (num1 > num2) {
+        return num2;
+    }
+    else {
+        return num2;
+    }
+}
+function isPalindrome(word) {
+    let rev = str.split("").reverse().join("");
+    if(rev === str) {
+        return true;
+    }
+    return false;
+}
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    let savedAmount = (originalPrice * discountPercentage /100);
+    let discount = originalPrice - savedAmount;
+    return discount;
+}
 
 
 
